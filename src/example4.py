cd57e58a3e1d81fadc2fe5a6578109359f6cee55
RitoAgent <mklepikov228@mail.ru>
2023-06-12 20:23:42 +0200

    cycle

--- src/example4.py
+++ src/example4.py
@@ -67,8 +67,8 @@ print(" ")
 word = 'thud'
 for letter in word:
     if letter == 'x':
-            print("Eek! An 'x'!")
-            break
+        print("Eek! An 'x'!")
+    break
     print(letter)
 else:
     print("No 'x' in there.")
--- src/forwhile.py
+++ src/forwhile.py
@@ -1,9 +1,10 @@
 # цикл for, чтобы вывести на экран значения списка [3, 2, 1, 0].
 print(" ")
-list(range(0, 3))
+#list(range(0, 3))
 for x in range(3, -1, -1):
     print(x)
 print(" ")
+
 # Присвойте значение 7 переменной guess_me и значение 1 переменной number.
 # Напишите цикл while, который сравнивает переменные number и guess_me.
 # Выведите строку 'too low', если значение переменной number меньше значе-
@@ -12,19 +13,21 @@ print(" ")
 # значение переменной number больше значения переменной guess_me, выведите
 # строку 'oops' и выйдите из цикла. Увеличьте значение переменной number на
 # выходе из цикла.
-number = 1
-guess_me = 7
-while number < guess_me:
-    number += 1
-    if number < guess_me:
-        continue
-    print('too low')
-    if number == guess_me:
-        break
-    print('found it')
+number = 0
+guess_me = 8
+start_text = input('Введите число для поиска: ')
+start = int(start_text)
+while number < start < guess_me :
+    start += 1
+    print('too short!', start - 1)
+    continue
+if start == guess_me :
+    print('found it:', guess_me - 1)
 else:
-    print('oops')
-print(" ")
+    print('oops!')
+         
+print(" ")  
+
 # print('No even number found')
 # Присвойте значение 5 переменной guess_me. Используйте цикл for для того,
 # чтобы проитерировать с помощью переменной number по диапазону range(10).
@@ -33,16 +36,12 @@ print(" ")
 # сообщение 'found it!', а затем выйдите из цикла. Если значение переменной
 # number больше, чем guess_me, выведите на экран сообщение 'oops' и выйдите
 # из цикла. 
-guess_me = 5
 number = range(10)
 for guess_me in number:
-    #number += 1
-   # if number < guess_me:
-   #     break
-    #print('too low')
-    if number == guess_me:
-        continue
-    print('found it')
-        
-#else:
-#    print('oops')
+    print('too short!', guess_me)
+    guess_me += 1
+    if guess_me == 5:
+        print('found it')
+        break
+else:
+    print('oops')
